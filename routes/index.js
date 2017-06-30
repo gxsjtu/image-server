d4047cfdcefd26d8ff1a90655d1e1697dbc8ba1f
郭轩 <guoxuan@guoxuan.local>
2017-06-30 15:10:31 +0800

    OK

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer')
-var upload = multer({dest: 'public/images/'})
+var upload = multer({
+  dest: 'public/images/'
+})
 var sharp = require('sharp');
 var path = require('path');
 const fs = require('fs');
@@ -10,36 +12,48 @@ const junk = require('junk');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.json({result: 'success'});
+  res.json({
+    result: 'success'
+  });
 });
 
 router.post('/upload', upload.single('file'), (req, res, next) => {
-    sharp(path.join(__dirname, '..', '/' + req.file.path)).resize(60, 60).toFile(path.join(__dirname, '..', '/' + req.file.path + '-1')).then(data => {
-        res.json({result: 'ok', fileName: req.file.filename});
-    }).catch(err => {
-        res.json({result: 'error'});
-    })
+  sharp(path.join(__dirname, '..', '/' + req.file.path)).resize(60, 60).toFile(path.join(__dirname, '..', '/' + req.file.path + '-1')).then(data => {
+    res.json({
+      result: 'ok',
+      fileName: req.file.filename
+    });
+  }).catch(err => {
+    res.json({
+      result: 'error'
+    });
+  })
 })
 
-router.get('/getADImage',function(req, res, next){
+router.get('/getADImage', function(req, res, next) {
   var path = __dirname + '/..' + '/public/ads';
   fs.readdir(path, (err, files) => {
     if (err) {
-      res.json({result: 'error'});
+      res.json({
+        result: 'error'
+      });
     } else {
-      var maxName = 0;//记录最大图片的名字
+      var maxName = 0; //记录最大图片的名字
       // var imgFileName;//防止图片后缀不同 所以存储起来
       _.forEach(files.filter(junk.not), x => {
         // var index = x.lastIndexOf('.');
         var imgName = parseInt(x);
-        if(imgName > maxName){
+        if (imgName > maxName) {
           maxName = imgName;
           // imgFileName = x;
         }
       })
       var imgBuf = fs.readFileSync(__dirname + '/..' + '/public/ads/' + maxName);
       var imgBase = imgBuf.toString("base64");
-      res.json({result: 'ok', imgData: imgBase});
+      res.json({
+        result: 'ok',
+        imgData: imgBase
+      });
       // res.json({result: 'ok', imgName: imgFileName});
     }
   })
